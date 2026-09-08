393a6827b2372da74061e6c8015637eca91a8f80
Eran Turgeman <erant@jfrog.com>
2026-09-08 18:01:20 +0300

    adding another defensive mechanism

--- integrationutils.go
+++ integrationutils.go
@@ -5,12 +5,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/go-git/go-git/v5"
 	"os"
 	"strconv"
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/go-git/go-git/v5"
 
 	githttp "github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/jfrog/froggit-go/vcsclient"
@@ -135,9 +136,9 @@ func findRelevantPrID(pullRequests []vcsclient.PullRequestInfo, branch string) (
 	return
 }
 
-func findRelevantPrIDByBranchPrefix(pullRequests []vcsclient.PullRequestInfo, branchPrefix string) (prId int, branchName string) {
+func findRelevantPrIDByBranchPrefix(pullRequests []vcsclient.PullRequestInfo, branchPrefix string, preExistingPrIDs map[int]bool) (prId int, branchName string) {
 	for _, pr := range pullRequests {
-		if strings.HasPrefix(pr.Source.Name, branchPrefix) && pr.Target.Name == mainBranch {
+		if strings.HasPrefix(pr.Source.Name, branchPrefix) && pr.Target.Name == mainBranch && !preExistingPrIDs[int(pr.ID)] {
 			return int(pr.ID), pr.Source.Name
 		}
 	}
@@ -218,6 +219,11 @@ func runScanRepositoryCmd(t *testing.T, client vcsclient.VcsClient, testDetails 
 	gitManager := buildGitManager(t, testDetails)
 	defer cleanupLeftoverFrogbotPRs(t, client, testDetails, gitManager)
 
+	preExistingPrIDs := make(map[int]bool)
+	for _, pr := range getOpenPullRequests(t, client, testDetails) {
+		preExistingPrIDs[int(pr.ID)] = true
+	}
+
 	err = Exec(&scanrepository.ScanRepositoryCmd{}, utils.ScanRepository)
 	require.NoError(t, err)
 
@@ -226,7 +232,7 @@ func runScanRepositoryCmd(t *testing.T, client vcsclient.VcsClient, testDetails 
 	expectedFixPackages := []string{"snyk", "lodash", "minimist"}
 	for _, pkg := range expectedFixPackages {
 		branchPrefix := scanRepoTestBranchNamePrefix + pkg + "-"
-		prId, branchName := findRelevantPrIDByBranchPrefix(pullRequests, branchPrefix)
+		prId, branchName := findRelevantPrIDByBranchPrefix(pullRequests, branchPrefix, preExistingPrIDs)
 		assert.NotZero(t, prId, "Expected to find PR for package %s", pkg)
 		if prId != 0 {
 			closePullRequest(t, client, testDetails, prId)
